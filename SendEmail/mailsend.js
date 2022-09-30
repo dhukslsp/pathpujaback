0ec153fb94de3f4f676cf4cb16cda0a19b4e1a6a
Dhruv SIngh <dhruv1647>
2022-10-01 04:18:25 +0530

    wnd mail done

--- SendEmail/mailsend.js
+++ SendEmail/mailsend.js
@@ -43,4 +43,48 @@ router.post("/send_email", async(req, res) => {
     });
     res.send("Mail Sent");
 })
+router.post("/send_email1", async(req, res) => {
+    var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "pathpuja.com@gmail.com",
+            pass: "cusmislepyatibhb"
+        }
+    });
+    transporter.use('compile', handle({
+        viewEngine: {
+            extname: ".handlebars",
+            partialsDir: path.resolve('./views'),
+            defaultLayout: false
+        },
+        viewPath: path.resolve('./views'),
+        extName: ".handlebars"
+
+    }));
+    var mailoperations = {
+        from: "pathpuja.com@gmail.com",
+        to: "pathpuja.com@gmail.com",
+        subject: "Congratulations!! Booking Confirmed.",
+        template: "CustumerConfirm",
+        context: {
+            CusfName: req.body.CusfName,
+            CuslName: req.body.CuslName,
+            Puja_Name: req.body.pujaname,
+            packageName: req.body.packageName,
+            performdate: req.body.PujaEventDate,
+            CusAdress:req.body.CusAdress,
+            Cuscity:req.body.Cuscity,
+            CusState:req.body.CusState,
+            CusPinCode: req.body.CusPinCode,
+            prices: req.body.pices*20/100,
+            performdate: req.body.PujaEventDate
+        }
+    }
+    transporter.sendMail(mailoperations, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.send("Mail Sent");
+})
 module.exports = router;
