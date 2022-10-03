1fdff8190e638906291f2a63df7cbc7477eb6005
Dhruv SIngh <dhruv1647>
2022-10-03 12:30:21 +0530

    newsetcommit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 2001;
 const connectToMongo = require("./db");
 const cors = require("cors");
-
+const port = 20;
 connectToMongo();
 app.use(express.json());
 app.use(cors());
@@ -12,5 +11,5 @@ app.use("/api/Fetch", require("./RetrievePuja/FetchCustdet"));
 app.use("/api/insert", require("./RetrievePuja/CustumrDet"));
 app.use("/api/send", require("./SendEmail/mailsend"));
 app.listen(port, () => {
-    console.log('App listning to port 2001');
+    console.log('App listning to port 20');
 })
